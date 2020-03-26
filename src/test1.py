2a65de94a10dde3bb02144a9605098d200afa5ce
daotongyang <2564933537@qq.com>
2020-03-26 15:52:32 +0800

    check

--- src/test1.py
+++ src/test1.py
@@ -10,11 +10,9 @@ def menu():
     print('*                                                                     3.按学号和时间查询                                                     *')
     print('*                                                                     4.按周查询总体                                                            *')
     print('*                                                                     5.按月查询总体                                                            *')
-    #print('*                                                                     6.添加人员                                                                   *')
-    #print('*                                                                     7.删除人员                                                                   *')
     print('*                                                                     111.退出系统                                                               *')
     print('********************************************************************************************************')
-
+#实现输入
 def init():
     menu()
     kind = int(input("请输入指令："))
@@ -22,20 +20,12 @@ def init():
 
 #按姓名查询
 def search_by_name(st_name):
-    # 打开数据库连接
+    # 数据库连接
     db = pymysql.connect("localhost","root","lb666666","user1" )
- 
-    # 使用 cursor() 方法创建一个游标对象 cursor
     cursor = db.cursor()
-
-    # 使用 execute()  方法执行 SQL 查询 
     sql = "SELECT * FROM  STUDENTS WHERE name = '%s'  " % (st_name)
-    
     try:
-        # 执行SQL语句
         cursor.execute(sql)
-        
-        # 获取所有记录列表
         results = cursor.fetchall()
         for row in results:
             st_id = row[0]
@@ -61,33 +51,18 @@ def search_by_name(st_name):
         print("是否与疫区人员接触：", st_disaster)
         print("是否返校：", st_arrive_school)
         print("签到时间：", st_time)
-        
-        #break
     except:
         print ("Error: unfind name!")
- 
-    # 关闭数据库连接
     db.close()
 
 #按学号查询
 def search_by_id(st_id):
-    # 打开数据库连接
-    #print(st_name)
-
     db = pymysql.connect("localhost","root","lb666666","user1" )
- 
-    # 使用 cursor() 方法创建一个游标对象 cursor
-    cursor = db.cursor()
-
-    # 使用 execute()  方法执行 SQL 查询 
+    cursor = db.cursor() 
     sql = "SELECT * FROM  STUDENTS WHERE id = '%s'  " % (st_id)
-    
-    #print(st_name)
     try:
         # 执行SQL语句
         cursor.execute(sql)
-        
-        # 获取所有记录列表
         results = cursor.fetchall()
         for row in results:
             st_id = row[0]
@@ -116,29 +91,15 @@ def search_by_id(st_id):
             
     except:
         print ("Error: unfind name!")
- 
-    # 关闭数据库连接
     db.close()
 
 
 def search_by_idAndtime(st_id, st_time):
-
-    # 打开数据库连接
-    #print(st_name)
-
     db = pymysql.connect("localhost","root","lb666666","user1" )
- 
-    # 使用 cursor() 方法创建一个游标对象 cursor
     cursor = db.cursor()
-
-    # 使用 execute()  方法执行 SQL 查询 
-    sql = "SELECT * FROM  STUDENTS WHERE DATE_FORMAT(arrive_school_time, '%%Y-%%m-%%d') = '%s' and  id = '%s' " % (st_time, st_id)
-    
+    sql = "SELECT * FROM  STUDENTS WHERE DATE_FORMAT(arrive_school_time, '%%Y-%%m-%%d') = '%s' and  id = '%s' " % (st_time, st_id)    
     try:
-        # 执行SQL语句
         cursor.execute(sql)
-        
-        # 获取所有记录列表
         results = cursor.fetchall()
         for row in results:
             st_id = row[0]
@@ -167,20 +128,11 @@ def search_by_idAndtime(st_id, st_time):
             
     except:
         print ("Error: unfind name!")
- 
-    # 关闭数据库连接
     db.close()
-
-
+#实现周查询
 def search_by_weekend(st_weekend):
-    
-    # 打开数据库连接
     db = pymysql.connect("localhost","root","lb666666","user1" )
- 
-    # 使用 cursor() 方法创建一个游标对象 cursor
     cursor = db.cursor()
-
-    # 使用 execute()  方法执行 SQL 查询
     sql = [ ]
     #s0是周的统计人数
     s0=  "SELECT COUNT(*) FROM STUDENTS \
@@ -212,31 +164,22 @@ def search_by_weekend(st_weekend):
     sql.append(s5)
     
     try:
-        # 执行SQL语句
         results = [ ]
         for i in range(0,len(sql)):
             result = 0
             cursor.execute(sql[i])
             result = cursor.fetchone()
-            #print(result)
             results.append(result)
         return results
     except:
         print ("Error: unfind!")
- 
-    # 关闭数据库连接
     db.close()
 
-
+#实现月查询
 def search_by_month(st_month):
     db = pymysql.connect("localhost","root","lb666666","user1" )
- 
-    # 使用 cursor() 方法创建一个游标对象 cursor
     cursor = db.cursor()
-
-    # 使用 execute()  方法执行 SQL 查询
     sql = [ ]
-    #s0是周的统计人数
     s0=  "SELECT COUNT(*) FROM STUDENTS \
             WHERE DATE_FORMAT(arrive_school_time, '%%Y-%%m')='%s' \
             GROUP BY DATE_FORMAT(arrive_school_time, '%%Y-%%m') " % (st_month)
@@ -275,11 +218,9 @@ def search_by_month(st_month):
         return results
     except:
         print ("Error: unfind!")
- 
-    # 关闭数据库连接
     db.close()
 
-
+#调用执行
 if __name__ == '__main__':
     kind = init()
     while True:
@@ -394,28 +335,15 @@ if __name__ == '__main__':
             num_list2 = rn      # 纵坐标值2
 
             m = range(len(num_list1))
-                
-            """
-            绘制条形图
-            x:长条形中点横坐标
-            height:长条形高度
-            width:长条形宽度，默认值0.8
-            label:为后面设置legend准备
-            """
             rects1 = plt.bar(x=m, height=num_list1, width=0.4, alpha=0.8, color='red', label="Yes")
             rects2 = plt.bar(x=[i + 0.4 for i in m], height=num_list2, width=0.4, color='green', label="No")
             plt.ylim(0, 20)     # y轴取值范围
             plt.ylabel("数量")
-            """
-            设置x轴刻度显示值
-            参数一：中点坐标
-            参数二：显示值
-            """
+            
             plt.xticks([index + 0.2 for index in m], label_list)
             plt.xlabel("类型")
             plt.title("月统计图")
-            plt.legend()     # 设置题注
-            # 编辑文本
+            plt.legend()     
             for rect in rects1:
                 height = rect.get_height()
                 plt.text(rect.get_x() + rect.get_width() / 2, height+1, str(height), ha="center", va="bottom")
